ff07f45555c2cd93533666ed73c55b4fb1db050f
Tomka Yuriy <tomka.yuriy@gmail.com>
2023-03-05 11:09:26 +0200

    01

--- FakeDataDbGenerator/Program.cs
+++ FakeDataDbGenerator/Program.cs
@@ -4,6 +4,7 @@ using FakeDataDriverDbGenerator.Seeders;
 var seeder = new DriverDatabaseSeeder();
 var commp = seeder.Companies;
 
+
 var context = new DriversManagementContext();
 context.Companies.AddRange(commp);
 context.SaveChanges();
