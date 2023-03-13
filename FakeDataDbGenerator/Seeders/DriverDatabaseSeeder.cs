4ef3be1f6ee21d5ce8e0d488d44640ebbb57681f
CrayDelay <vitlana.belei@gmail.com>
2023-03-13 19:00:58 +0200

    13.03.2023

--- FakeDataDbGenerator/Seeders/DriverDatabaseSeeder.cs
+++ FakeDataDbGenerator/Seeders/DriverDatabaseSeeder.cs
@@ -25,11 +25,20 @@ namespace FakeDataDriverDbGenerator.Seeders
         public IReadOnlyCollection<Company> Companies { get; } = new List<Company>();
         //public IReadOnlyCollection<Driver> Drivers { get; } new List<Driver>();
         public IReadOnlyCollection<Expense> Expenses { get; } new List<Expense>();
+        public IReadOnlyCollection<Inspection> Inspections { get; } new List<Inspection>();
+        public IReadOnlyCollection<Repair> Repairs { get; } new List<Repair>();
+        public IReadOnlyCollection<Truck> Trucks { get; } new List<Truck>();
+        public IReadOnlyCollection<Photo> Photos { get; } new List<Photo>();
+
         public DriverDatabaseSeeder()
         {
             Companies = GenerateCompanies(amount: 20);
             //Drivers = GenerateDrivers(amount: 20);
             Expenses = GenerateExpenses(amount: 20);
+            Inspections = GenerateInspections(amount: 20);
+            Repairs = GenerateRepairs(amount: 20);
+            Trucks = GenerateTrucks(amount: 20);
+            Photos = GeneratePhotos(amount: 20);
         }
 
         private IReadOnlyCollection<Company> GenerateCompanies(int amount)
@@ -72,13 +81,80 @@ namespace FakeDataDriverDbGenerator.Seeders
             var ExpenseId = 1;
             var ExpenseFaker = new Faker<Expense>()
                 //.RuleFor(c => c.ExpensesId, f => f.ExpenseId++)
-                //.RuleFor(c => c.DriverId, )
+                //.RuleFor(c => c.DriverId, f => f.)
                 .RuleFor(c => c.DriverPayment, f => f.Finance.Amount())
                 .RuleFor(c => c.FuelCost, f => f.Finance.Amount())
                 .RuleFor(c => c.MaintenanceCost, f => f.Finance.Amount())
                 //.RuleFor(c => c.Category, f => f.PickRandom<ExpenseCategory>())
                 .RuleFor(c => c.Date, f => f.Date.Past())
-                .RuleFor(c => c.Note, f => f.)
+                .RuleFor(c => c.Note, f => f.Rant.Review())
+                .RuleFor(c => c.CreatedAt, f => f.Date.Past())
+                .RuleFor(c => c.UpdatedAt, f => null as DateTime?)
+                .RuleFor(c => c.DeletedAt, f => null as DateTime?)
+                .RuleFor(c => c.Driver, f => null)
+                .RuleFor(c => c.Truck, f => null);
+
+        }
+
+        private IReadOnlyCollection<Inspection> GenerateInspections(int amount)
+        {
+            var InspectionId = 1;
+            var InspectionFaker = new Faker<Inspection>()
+                .RuleFor(c => c.InspectionDate, f => f.Date.Past())
+                .RuleFor(c => c.Description, f => f.Lorem.Text())
+                .RuleFor(c => c.Result, f => f.Random.Bool())
+                //.RuleFor(c => c.TruckId, f => f)
+                .RuleFor(c => c.CreatedAt, f => f.Date.Past())
+                .RuleFor(c => c.UpdatedAt, f => null as DateTime?)
+                .RuleFor(c => c.DeletedAt, f => null as DateTime?)
+                .RuleFor(c => c.Truck, f => null);
+        }
+
+        private IReadOnlyCollection<Repair> GenerateRepairs(int amount)
+        {
+            var RepairId = 1;
+            var RepairFaker = new Faker<Repair>()
+                .RuleFor(c => c.RepairDate, f => f.Date.Past())
+                .RuleFor(c => c.Description, f => f.Lorem.Text())
+                .RuleFor(c => c.Cost, f => f.Finance.Amount())
+                //.RuleFor(c => c.TruckId, f => )
+                .RuleFor(c => c.CreatedAt, f => f.Date.Past())
+                .RuleFor(c => c.UpdatedAt, f => null as DateTime?)
+                .RuleFor(c => c.DeletedAt, f => null as DateTime?)
+                .RuleFor(c => c.Truck, f => null);
+        }
+        private IReadOnlyCollection<Truck> GenerateTrucks(int amount)
+        {
+            var TruckId = 1;
+            var TruckFaker = new Faker<Truck>()
+                 .RuleFor(c => c.TruckNumber, f => f.Vehicle.Vin())
+                 .RuleFor(c => c.Model, f => f.Vehicle.Model())
+                 //.RuleFor(c => c.Year, f => f.Date.Past())//   string? -> int 
+                 .RuleFor(c => c.Capacity, f => f.Random.Number(100, 1000))
+                 .RuleFor(c => c.FuelType, f => f.Vehicle.Fuel())
+                 .RuleFor(c => c.FuelConsumption, f => f.Random.Decimal(1, 5))
+                 .RuleFor(c => c.RegistrationNumber, f => f.)
+                 .RuleFor(c => c.Vin, f => f.Vehicle.Vin())
+                 .RuleFor(c => c.EngineNumber, f => f.)
+                 .RuleFor(c => c.ChassisNumber, f => f.)
+                 .RuleFor(c => c.InsurancePolicyNumber, f => f.)
+                 .RuleFor(c => c.InsuranceExpirationDate, f => f.Date.Future)
+                 .RuleFor(c => c.CreatedAt, f => f.Date.Past())
+                 .RuleFor(c => c.UpdatedAt, f => null as DateTime?)
+                 .RuleFor(c => c.DeletedAt, f => null as DateTime?);
+        }
+        private IReadOnlyCollection<Photo> GeneratePhotos(int amount)
+        {
+            var PhotoId = 1;
+            var PhotoFaker = new Faker<Photo>()
+                .RuleFor(c => c.PhotoData, f => f.Random.Bytes())
+                .RuleFor(c => c.ContentType, f => f.)
+                .RuleFor(c => c.FileName, f => f.Lorem.Word)
+                .RuleFor(c => c.FileSize, f => f.Random.Number(0, 5000))
+                .RuleFor(c => c.CreatedAt, f => f.Date.Past())
+                .RuleFor(c => c.UpdatedAt, f => null as DateTime?)
+                .RuleFor(c => c.DeletedAt, f => null as DateTime?)
+                .RuleFor(c => c.Driver, f => null);
 
         }
 
